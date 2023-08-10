ee9cfddbf1c1bcb417432ccbcbe2c1a7d97eb830
chenjunqi <junqiqi77_chen@foxmail.com>
2023-08-10 17:30:28 +0800

    init

---
+++ src/main/java/com/example/javapractice/impl/SearchServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.javapractice.impl;
+
+import com.example.javapractice.SearchService;
+import com.example.javapractice.entity.Dog;
+
+/**
+ * @author: Junqi Chen
+ * @time:2023/8/10
+ * @Email:junqiqi77_chen@foxmail.com
+ * @desc:
+ */
+public class SearchServiceImpl implements SearchService {
+
+    @Override
+    public int getCount() {
+        var i = "fdfdf";
+        var dog = new Dog();
+        System.out.println(dog.getId());
+        // 写一个冒泡算法
+
+
+        return 0;
+    }
+}
---
+++ src/test/java/com/example/javapractice/JavaPracticeApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.javapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello junqi");
+        var i = "ewrr";
+        var k = 5;
+        System.out.println(i);
+        System.out.println(k);
+    }
+
+}
