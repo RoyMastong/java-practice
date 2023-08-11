10c798db2fda8f6a5eea360d292676145831d134
chenjunqi <junqiqi77_chen@foxmail.com>
2023-08-12 03:19:58 +0800

    add icu4j pom

--- src/test/java/com/example/javapractice/JavaPracticeApplicationTests.java
+++ src/test/java/com/example/javapractice/JavaPracticeApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.javapractice;
 
+import com.ibm.icu.text.Transliterator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,12 +8,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 class JavaPracticeApplicationTests {
 
     @Test
-    void contextLoads() {
+    void test1() {
         System.out.println("hello junqi");
         var i = "ewrr";
         var k = 5;
         System.out.println(i);
         System.out.println(k);
+
+        String rule = "Any-Latin;Latin-Devanagari"; // 翻译规则
+        String sourceText = "Hello, world!"; // 源文本
+
+        Transliterator transliterator = Transliterator.getInstance(rule);
+        String translatedText = transliterator.transliterate(sourceText);
+
+        System.out.println("Translated text: " + translatedText);
     }
 
 }
