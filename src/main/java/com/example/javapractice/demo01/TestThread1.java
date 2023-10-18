d3be19ab3d68b8d69b3e0ebd9b01e0b29fa1b114
junqi.chen <junqiqi77_chen@foxmail.com>
2023-10-19 04:34:35 +0800

    add thread

---
+++ src/main/java/com/example/javapractice/demo01/TestThread1.java
@@ -0,0 +1,42 @@
+package com.example.javapractice.demo01;
+
+/**
+ * @author: Junqi Chen
+ * @time:2023/10/19
+ * @Email:junqiqi77_chen@foxmail.com
+ * @desc:
+ */
+
+// 创建线程方式一：继承Thread类，重写run方法，调用start开启线程
+// 总结：注意，线程开启不一定立即执行，由cpu调用执行
+
+
+public class TestThread1 extends Thread {
+
+    @Override
+    public void run() {
+        //run方法线程体
+        for (int i = 0; i < 20; i++) {
+            System.out.println("我在看代码---" + i);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        // main线程，主线程
+
+        // 创建一个线程对象
+        TestThread1 testThread1 = new TestThread1();
+
+        // 调用start()方法开启线程
+        testThread1.start();
+
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("我在学习多线程--" + i);
+        }
+    }
+
+
+}
