8d5381171c51d388066705e81283876564f15be1
junqi.chen <junqiqi77_chen@foxmail.com>
2023-10-19 06:03:06 +0800

    add thread123

---
+++ src/main/java/com/example/javapractice/demo01/TestThread2.java
@@ -0,0 +1,59 @@
+package com.example.javapractice.demo01;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * @author: Junqi Chen
+ * @time:2023/10/19
+ * @Email:junqiqi77_chen@foxmail.com
+ * @desc:
+ */
+
+// 练习Thread，实现多线程同步下载图片
+public class TestThread2 implements Runnable {
+
+    private String url; // 网络图片地址
+    private String name; // 保存的文件名
+
+    public TestThread2(String url, String name) {
+        this.url = url;
+        this.name = name;
+    }
+
+    // 下载图片线程的执行体
+    @Override
+    public void run() {
+        WebDownloader webDownloader = new WebDownloader();
+        webDownloader.downloader(url, name);
+        System.out.println("下载了文件名为：" + name);
+    }
+
+    public static void main(String[] args) {
+        // 给一个图片能打开的网址，不要这个https://img-blog.csdnimg.cn/20211019105948109.png，这个打不开
+
+        TestThread2 t1 = new TestThread2("https://upload-images.jianshu.io/upload_images/2411388-b39ceb1c2dc5a569.png", "1.png");
+        TestThread2 t2 = new TestThread2("https://upload-images.jianshu.io/upload_images/2411388-d80b6825cd00fb0d.png", "2.png");
+        TestThread2 t3 = new TestThread2("https://upload-images.jianshu.io/upload_images/2411388-654b2a938885224c.png", "3.png");
+        new Thread(t1).start();
+        new Thread(t2).start();
+        new Thread(t3).start();
+    }
+}
+
+
+// 下载器
+class WebDownloader {
+    // 下载方法
+    public void downloader(String url, String name) {
+        try {
+            FileUtils.copyURLToFile(new URL(url), new File(name));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IO异常，downloader方法出现问题");
+        }
+    }
+}
--- src/main/java/com/example/javapractice/demo01/TestThread3.java
+++ src/main/java/com/example/javapractice/demo01/TestThread3.java
@@ -6,12 +6,8 @@ package com.example.javapractice.demo01;
  * @Email:junqiqi77_chen@foxmail.com
  * @desc:
  */
-
-// 创建线程方式一：继承Thread类，重写run方法，调用start开启线程
-// 总结：注意，线程开启不一定立即执行，由cpu调用执行
-
-
-public class TestThread1 extends Thread {
+// 创建线程方式2：实现Runnable接口，重写run方法，执行线程需要丢入Runnable接口实现类，调用start方法
+public class TestThread3 implements Runnable {
 
     @Override
     public void run() {
@@ -22,18 +18,14 @@ public class TestThread1 extends Thread {
     }
 
     public static void main(String[] args) {
-        // main线程，主线程
-
-        // 创建一个线程对象
-        TestThread1 testThread1 = new TestThread1();
-
-        // 调用start()方法开启线程
-        testThread1.start();
+        // 创建Runnable接口的实现类对象
+        TestThread3 testThread3 = new TestThread3();
+        // 创建线程对象，通过线程对象来开启我们的线程，代理
+        new Thread(testThread3).start();
 
 
         for (int i = 0; i < 20; i++) {
             System.out.println("我在学习多线程--" + i);
         }
     }
-
 }
