58bb6e32db9b7a0713c6472f1c8db62dfe3f6f74
junqi.chen <junqiqi77_chen@foxmail.com>
2023-10-20 16:03:51 +0800

    add TestJoin.java

---
+++ src/main/java/com/example/javapractice/multithread/state/TestJoin.java
@@ -0,0 +1,20 @@
+package com.example.javapractice.multithread.state;
+
+/**
+ * @author: Junqi Chen
+ * @time:2023/10/20
+ * @Email:junqiqi77_chen@foxmail.com
+ * @desc:
+ */
+// 测试join方法，想象为插队
+public class TestJoin implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/example/javapractice/multithread/state/TestSleep2.java
+++ src/main/java/com/example/javapractice/multithread/state/TestSleep2.java
@@ -1,10 +1,40 @@
 package com.example.javapractice.multithread.state;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author: Junqi Chen
  * @time:2023/10/20
  * @Email:junqiqi77_chen@foxmail.com
  * @desc:
  */
+// 模拟倒计时
 public class TestSleep2 {
+
+    public static void main(String[] args) {
+        // 获取当前系统时间
+        Date startTime = new Date(System.currentTimeMillis());
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                System.out.println(new SimpleDateFormat("HH:mm:ss").format(startTime));
+                startTime = new Date(System.currentTimeMillis()); // 更新当前时间
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // 模拟倒计时
+    public static void tenDown() throws InterruptedException {
+        int num = 10;
+        while (true) {
+            Thread.sleep(1000);
+            System.out.println(num--);
+            if (num <= 0) {
+                break;
+            }
+        }
+    }
 }
